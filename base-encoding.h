b11bca25e847541884174fc2fd3e03cfe922ce52
chardon55 <charlesdong_2000@outlook.com>
2022-08-17 23:11:09 +0800

    Update headers

--- base-encoding.h
+++ base-encoding.h
@@ -1,9 +1,28 @@
 #ifndef LIBPGFE_BASE_ENCODING_H
 #define LIBPGFE_BASE_ENCODING_H
 
+#include "generic.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+// Base 64
+size_t pgfe_encode_base64(const pgfe_encode_t input[], size_t input_length, char cs_out[]);
+
+size_t pgfe_decode_base64(const char base64_cs[], pgfe_encode_t output[]);
+
+// Base 32
+
+size_t pgfe_encode_base32(const pgfe_encode_t input[], size_t input_length, char cs_out[]);
+
+size_t pgfe_decode_base32(const char base32_cs[], pgfe_encode_t output[]);
+
+// Base 16
+
+size_t pgfe_encode_base16(const pgfe_encode_t input[], size_t input_length, char cs_out[]);
+
+size_t pgfe_decode_base16(const char base16_cs[], pgfe_encode_t output[]);
 
 #ifdef __cplusplus
 }
