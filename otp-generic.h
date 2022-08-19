f895c783e9cf641d5496f22afd7c5bc8f10c789b
chardon55 <charlesdong_2000@outlook.com>
2022-08-19 18:06:21 +0800

    Add comment header

--- otp-generic.h
+++ otp-generic.h
@@ -1,3 +1,20 @@
+/*
+  libpgfe
+  otp-generic.h
+
+  Copyright (C) 2022 Charles Dong
+
+  libpgfe is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+*/
+
 #ifndef LIBPGFE_OTP_GENERIC_H
 #define LIBPGFE_OTP_GENERIC_H
 
--- sha384.c
+++ sha384.c
@@ -1,3 +1,20 @@
+/*
+  libpgfe
+  sha384.c
+
+  Copyright (C) 2022 Charles Dong
+
+  libpgfe is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+*/
+
 #include "sha2.h"
 #include "templates.c"
 
