088830d0a73be94c2f6b7bf45c5115e8ac5b3479
chardon55 <charlesdong_2000@outlook.com>
2022-08-26 13:06:44 +0800

    Update version

--- meta.h
+++ meta.h
@@ -11,8 +11,8 @@
 #define LIBPGFE_NAME "libpgfe"
 
 #define LIBPGFE_MAJOR_VERSION 0
-#define LIBPGFE_MINOR_VERSION 2
+#define LIBPGFE_MINOR_VERSION 3
 #define LIBPGFE_REVISION 0
 
-#define LIBPGFE_VERSION "0.2.0"
+#define LIBPGFE_VERSION "0.3.0"
 #endif
