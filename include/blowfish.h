31bd544c287656aac4e3582083ad09d401fa2356
chardon55 <charlesdong_2000@outlook.com>
2022-10-03 21:04:32 +0800

    Update blowfish

--- include/blowfish.h
+++ include/blowfish.h
@@ -4,6 +4,26 @@
 extern "C" {
 #endif
 
+#include "generic-internal.h"
+
+#define __PGFE_BF_N 16
+#define __PGFE_BF_Np2 18
+#define __PGFE_BF_S_ROW 4
+#define __PGFE_BF_S_ENTRY 256
+
+typedef uint32_t pgfe_blowfish_block_t;
+
+struct pgfe_blowfish_ctx
+{
+    pgfe_blowfish_block_t P[__PGFE_BF_Np2], S[__PGFE_BF_S_ROW][__PGFE_BF_S_ENTRY];
+};
+
+void pgfe_blowfish_init(struct pgfe_blowfish_ctx *ctx, pgfe_encode_t key[], size_t key_length);
+
+void pgfe_blowfish_encrypt(struct pgfe_blowfish_ctx *ctx, uint64_t *input);
+
+void pgfe_blowfish_decrypt(struct pgfe_blowfish_ctx *ctx, uint64_t *input);
+
 #ifdef __cplusplus
 }
 #endif
