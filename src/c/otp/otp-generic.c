11c0613bdfc605f5439df416de5cf8cdafaaf365
chardon55 <charlesdong_2000@outlook.com>
2022-11-15 19:17:11 +0800

    Optimization: Use macro

--- src/c/otp/otp-generic.c
+++ src/c/otp/otp-generic.c
@@ -7,13 +7,15 @@
 
 #include "backend/otp-generic.h"
 
+#define __ENS_BIT 8
+#define __SIZE 4
+
 pgfe_otp_t pgfe_dynamically_truncate(const pgfe_encode_t hash[], size_t length) {
     uint8_t offset = hash[length - 1] & 0xF;
     pgfe_otp_t code = 0;
 
-    size_t ens = sizeof(pgfe_encode_t), sz = sizeof(pgfe_otp_t) / ens;
-    for (size_t i = 0; i < sz; i++) {
-        code <<= to_bit(ens);
+    for (size_t i = 0; i < __SIZE; i++) {
+        code <<= __ENS_BIT;
         code |= hash[offset + i];
     }
 
