43098ffbadf4669f349ba2cd23b077b98a7c0802
chardon55 <charlesdong_2000@outlook.com>
2023-06-20 07:58:01 +0800

    Update version

--- include/libpgfe/version.h
+++ include/libpgfe/version.h
@@ -1,18 +1,11 @@
-/*
-  libpgfe
-  version.h
-
-  Copyright (c) 2022-2023 Charles Dong
-*/
-
 #ifndef LIBPGFE_VERSION_H
 #define LIBPGFE_VERSION_H
 
 #define LIBPGFE_MAJOR_VERSION 0
 #define LIBPGFE_MINOR_VERSION 7
-#define LIBPGFE_REVISION 0
+#define LIBPGFE_REVISION 1
 #define LIBPGFE_VARIANT "dev"
 
-#define LIBPGFE_VERSION "0.7.0-dev"
+#define LIBPGFE_VERSION "0.7.1-dev"
 
 #endif
--- metadata.mak
+++ metadata.mak
@@ -1,5 +1,5 @@
 PROJECT := libpgfe
-VERSION := 0.7.0-dev
+VERSION := 0.7.1-dev
 
 BUILD_DIR := build
 
